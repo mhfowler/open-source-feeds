8227883d588900e4de20882abd2f5250e08087c6
Max Fowler <maxhfowler@gmail.com>
2017-07-22 13:45:04 -0400

    OSF version 0.0.2

--- osf_scraper_api/scripts/test.sh
+++ osf_scraper_api/scripts/test.sh
@@ -2,6 +2,6 @@
 BASEDIR=$( cd $(dirname $0) ; pwd -P )
 echo $BASEDIR
 #curl -X PUT -H "Content-Type: application/json" --data-binary "@${BASEDIR}/scrape_posts.json" "http://127.0.0.1:5002/api/scrape_posts/"
-curl -X PUT -H "Content-Type: application/json" --data-binary "@${BASEDIR}/scrape_friends.json" "http://127.0.0.1:5002/api/scrape_friends/"
+#curl -X PUT -H "Content-Type: application/json" --data-binary "@${BASEDIR}/scrape_friends.json" "http://127.0.0.1:5002/api/scrape_friends/"
 #curl -X PUT -H "Content-Type: application/json" --data-binary "@${BASEDIR}/test.json" "http://localhost/api/scrape_posts/"
-#curl -X PUT -H "Content-Type: application/json" --data-binary "@${BASEDIR}/scrape_posts.json" "http://54.226.31.5/api/scrape_posts/"
+curl -X PUT -H "Content-Type: application/json" --data-binary "@${BASEDIR}/scrape_posts.json" "http://api.opensourcefeeds.com/api/scrape_posts/"
