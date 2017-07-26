1a59f47c3c52686226b996d3d00228fa47259729
Max Fowler <maxhfowler@gmail.com>
2017-07-25 21:32:12 -0400

    Parse fb posts

--- osf/osf/scrapers/facebook/fbscrape.py
+++ osf/osf/scrapers/facebook/fbscrape.py
@@ -10,6 +10,95 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 BASE_URL = 'https://www.facebook.com'
 
 
+class Post:
+    """
+    wrapper for searching and interacting with facebook post element as retrieved by selenium
+    """
+    def __init__(self, selenium_element):
+        self.element = selenium_element
+        link_divs = self.element.find_elements_by_css_selector('a._5pcq')
+        if link_divs:
+            self.link_div = link_divs[0]
+        else:
+            self.link_div = None
+
+    def is_valid(self):
+        return self.link_div is not None
+
+    def get_element(self):
+        return self.element
+
+    def get_timestamp(self):
+        post_content = self.link_div.get_attribute('innerHTML')
+        match = re.match('.*data-utime="(\d+)"', post_content)
+        if match:
+            timestamp = int(match.group(1))
+            return timestamp
+        else:
+            return None
+
+    def get_link(self):
+        self.link = self.link_div.get_attribute('href')
+        return self.link
+
+    def get_content(self):
+
+        # store whatever we can get in here
+        content = {}
+
+        # try to get author
+        author_div = self.element.find_elements_by_css_selector('.fwb')
+        if author_div:
+            author_div = author_div[0]
+            author_a = author_div.find_elements_by_css_selector('a')
+            if author_a:
+                author_a = author_a[0]
+                author_href = author_a.get_attribute('href')
+                match = re.match('https://www.facebook.com/([^?]+)\??.*', author_href)
+                if match:
+                    author = match.group(1)
+                else:
+                    author = author_href
+                content['author'] = author
+
+        # try to get text of post
+        user_text_div = self.element.find_elements_by_css_selector('.userContent')
+        if user_text_div:
+            user_text_div = user_text_div[0]
+            user_text = user_text_div.text
+            content['text'] = user_text
+
+        # try to find article
+        article_div = self.element.find_elements_by_css_selector('._52c6')
+        if article_div:
+            article = article_div[0]
+            link = article.get_attribute('href')
+            content['link'] = link
+
+        # try to get images
+        image_divs = self.element.find_elements_by_css_selector('._4-eo')
+        if image_divs:
+            image_links = []
+            for image_div in image_divs:
+                image_link = image_div.get_attribute('href')
+                image_links.append(image_link)
+            if image_links:
+                content['images'] = image_links
+
+        # try to get events
+        event_divs = self.element.find_elements_by_css_selector('._fwx')
+        if event_divs:
+            event_div = event_divs[0]
+            event_a = event_div.find_elements_by_css_selector('a')
+            if event_a:
+                event_a = event_a[0]
+                event_link = event_a.get_attribute('href')
+                content['event'] = event_link
+
+        # return any content we found
+        return content
+
+
 class FbScraper():
     """
     Wrapper class for scraping facebook
@@ -96,7 +185,7 @@ class FbScraper():
         # return usernames
         return usernames
 
-    def get_posts_by_user(self, user):
+    def get_posts_by_user(self, user, after_date=None, max_num_posts_per_user=None):
         """
         fetches posts by a particular user
         :param user:
@@ -105,8 +194,11 @@ class FbScraper():
         if not self.logged_in:
             self.fb_login()
 
-        # list to store posts in
-        to_return = []
+        # convert after_date to timestamp
+        if after_date:
+            after_timestamp = time.mktime(after_date.timetuple())
+        else:
+            after_timestamp = None
 
         # navigate to the url of the friend
         url = '{}/{}'.format(BASE_URL, user)
@@ -114,30 +206,82 @@ class FbScraper():
         self.driver.get(url)
         time.sleep(4)
 
-        # scroll the page a few times
-        for i in range(0, 4):
-            self.log('++ scrolling')
-            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(1)
+        # scroll the page and scrape posts
+        posts = {}
+        finished = False
+        num_consecutive_searches_without_posts = 0
+        num_searches = 0
+        max_num_searches = 200
+        # keep looping until we reach a finished condition
+        # (no new posts, > than oldest date, or > max_num_searches)
+        while not finished and (num_searches < max_num_searches):
 
-        # grab the posts
-        posts = self.driver.find_elements_by_css_selector('a._5pcq')
-        for post in posts:
-            post_link = post.get_attribute('href')
-            post_content = post.get_attribute('innerHTML')
-            match = re.match('.*data-utime="(\d+)"', post_content)
-            if match:
-                timestamp = int(match.group(1))
-                d = datetime.datetime.fromtimestamp(timestamp)
-                # TODO: do some confirmation, post is beyond certain date
-                to_return.append({
-                    'link': post_link,
-                    'date': timestamp
-                })
+            # increment search counter
+            num_searches += 1
+
+            # scroll down the page a few times
+            for i in range(0, 4):
+                self.log('++ scrolling')
+                self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(1)
+
+            # grab the posts
+            # found_posts = self.driver.find_elements_by_css_selector('a._5pcq')
+            found_sel_posts = self.driver.find_elements_by_css_selector('div.fbUserContent')
+            found_posts = [Post(x) for x in found_sel_posts]
+            # filter out malformed posts
+            found_posts = filter(lambda p: p.is_valid(), found_posts)
+
+            # only consider new posts
+            new_posts = filter(lambda p: p.get_link() not in posts, found_posts)
+
+            # if no new posts, increment counter
+            if not new_posts:
+                num_consecutive_searches_without_posts += 1
+                # if there have been 3 searches in a row without any new posts, then break the loop
+                if num_consecutive_searches_without_posts >= 3:
+                    finished = True
             else:
-                pass
+                self.log('++ found {} posts'.format(len(new_posts)))
 
-        # finally return all the posts as a list
+            # calculate how many posts
+            num_posts = len(posts.keys())
+
+            # if max_num_posts_per_user, check if we have exceeded the limit
+            if max_num_posts_per_user:
+                num_posts_remaining = max_num_posts_per_user - num_posts
+                if len(new_posts) > num_posts_remaining:
+                    # end the for loop
+                    finished = True
+                    # and truncate new_posts
+                    new_posts = new_posts[:num_posts_remaining]
+                    self.log('++ stopping search due to max_num_posts_per_user')
+
+            # iterate through new posts and parse and save them
+            for post in new_posts:
+                post_link = post.get_link()
+                timestamp = post.get_timestamp()
+                if timestamp:
+                    # if we found a post older than the given date, then stop downloading
+                    if after_timestamp and timestamp < after_timestamp:
+                        finished = True
+                    # otherwise add the post to the list of found posts
+                    else:
+                        if post_link not in posts:
+                            posts[post_link] = {
+                                'link': post_link,
+                                'date': timestamp,
+                                'content': post.get_content()
+                            }
+                else:
+                    pass
+
+        # convert dictionary of posts to a list
+        to_return = []
+        for post_link, post in posts.items():
+            to_return.append(post)
+
+        # return the list of posts
         return to_return
 
     def fb_login(self):
@@ -173,7 +317,10 @@ class FbScraper():
         posts = {}
         self.output['posts'] = posts
         for user in users:
-            user_posts = self.get_posts_by_user(user)
+            user_posts = self.get_posts_by_user(user,
+                                                after_date=params.get('after_date'),
+                                                max_num_posts_per_user=params.get('max_num_posts_per_user')
+                                                )
             # store the posts in a dictionary which will be written to output later
             posts[user] = user_posts
 
@@ -192,5 +339,10 @@ if __name__ == '__main__':
         fb_password=ENV_DICT['FB_PASSWORD'],
         command_executor=None)
     import json
-    print json.dumps(fbscraper.get_posts({'users': ENV_DICT['FB_FRIENDS']}))
+    output = fbscraper.get_posts({
+        'users': ENV_DICT['FB_FRIENDS'],
+        'after_date': datetime.datetime.now() - datetime.timedelta(days=365),
+        'max_num_posts_per_user': 15,
+    })
+    print json.dumps(output)
 
