843dfd8a15238a8c14c506156323835586e13de2
Max Fowler <maxhfowler@gmail.com>
2017-09-27 00:03:11 -0400

    Write error messages to S3

--- osf_scraper_api/osf_scraper_api/jobs/run_job.py
+++ osf_scraper_api/osf_scraper_api/jobs/run_job.py
@@ -1,6 +1,6 @@
 import time
 
-from osf_scraper_api.utilities.log_helper import _log
+from osf_scraper_api.utilities.log_helper import _log, _capture_exception
 from osf_scraper_api.utilities.fs_helper import save_dict
 from osf_scraper_api.jobs.scrape_fb_posts import scrape_fb_posts
 from osf_scraper_api.jobs.scrape_fb_friends import scrape_fb_friends
@@ -18,22 +18,34 @@ def run_job(job_type, job_params, output_bin):
             log_params[key] = val
             _log('++ param[{}]: {}'.format(key, val))
 
-    # run the job
+    # choose which job to run
     job_funs = {
         'scrape_fb_posts': scrape_fb_posts,
         'scrape_fb_friends': scrape_fb_friends,
     }
     job_fun = job_funs[job_type]
-    data_dict = job_fun(**job_params)
 
-    # format output
-    timestamp = int(time.time())
-    output_dict = {
-        'job_type': job_type,
-        'job_params': log_params,
-        'created_at': timestamp,
-        'data': data_dict
-    }
+    # run the job
+    try:
+        data_dict = job_fun(**job_params)
+        # format the output
+        timestamp = int(time.time())
+        output_dict = {
+            'job_type': job_type,
+            'job_params': log_params,
+            'created_at': timestamp,
+            'data': data_dict
+        }
+    except Exception as e:
+        # if there was an error, then still write the output, but with the error message
+        _capture_exception(e)
+        timestamp = int(time.time())
+        output_dict = {
+            'job_type': job_type,
+            'job_params': log_params,
+            'created_at': timestamp,
+            'error': e.message
+        }
 
     # save the output to the bin (with a filename of the current time)
     output_path = 'output/{}/{}.json'.format(output_bin, timestamp)
